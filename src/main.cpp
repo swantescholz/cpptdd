0b865b2921e07c19bce416b43fb605eff5386966
swantescholz <swante_scholz@web.de>
2013-04-08 01:40:48 +0200

    modified main.cpp test to fail

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
-
 #include "examples/Example.h"
 #include "tdd/tdd.h"
 
-
-//you can put tests in your main.cpp file if you really want to
+//you can put tests in your main.cpp file if you want to
 Test(one test from main.cpp) {
 	int x = 9;
-	assertEqual(x,9)
+	assertEqual(x, 8)
 }
 
 // =======================
@@ -14,7 +12,7 @@ Test(one test from main.cpp) {
 
 //EITHER manually configure how and which tests to run:
 int main(int argc, char **argv) {
-	testing.setOutputWidth(100); //the length of the seperation lines
+	testing.setOutputWidth(60); //the length of the seperation lines
 	testing.setDefaultTestrunResultIndentation(50); //controls the maximum number of dots in "... done"
 	testing.runAllTests(); //this will run all testfiles in alphabetical order
 	return 0;
